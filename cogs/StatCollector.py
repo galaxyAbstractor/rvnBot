0894808d30a069dbe2b0b8ee4e644645795bacff
galaxyAbstractor <galaxyAbstractor@gmail.com>
2019-05-11 15:57:12 +0200

    Added annotations

--- cogs/StatCollector.py
+++ cogs/StatCollector.py
@@ -2,12 +2,13 @@ from discord import TextChannel
 from discord.ext import commands
 
 
-class StatCollector(commands.cog):
+class StatCollector(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
         self._last_result = None
 
+    @commands.Cog.listener()
     async def on_message(self, message):
         if message.author == self.user:
             return
@@ -15,6 +16,7 @@ class StatCollector(commands.cog):
         if not isinstance(message.channel, TextChannel):
             return
 
+    @commands.Cog.listener()
     async def on_typing(self, channel, user, when):
         if user == self.user:
             return
@@ -22,41 +24,52 @@ class StatCollector(commands.cog):
         if not isinstance(channel, TextChannel):
             return
 
+    @commands.Cog.listener()
     async def on_raw_message_delete(self, payload):
         message = payload.cached_message
 
         return
 
+    @commands.Cog.listener()
     async def on_raw_bulk_message_delete(self, payload):
         messages = payload.cached_messages
 
         return
 
+    @commands.Cog.listener()
     async def on_raw_message_edit(self, payload):
         messages = payload.cached_messages
 
         return
 
+    @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
         return
 
+    @commands.Cog.listener()
     async def on_reaction_remove(self, reaction, user):
         return
 
+    @commands.Cog.listener()
     async def on_member_join(self, member):
         return
 
+    @commands.Cog.listener()
     async def on_member_remove(self, member):
         return
 
+    @commands.Cog.listener()
     async def on_member_update(self, member):
         return
 
+    @commands.Cog.listener()
     async def on_user_update(self, user):
         return
 
+    @commands.Cog.listener()
     async def on_member_ban(self, guild, user):
         return
 
+    @commands.Cog.listener()
     async def on_member_unban(self, guild, user):
         return
